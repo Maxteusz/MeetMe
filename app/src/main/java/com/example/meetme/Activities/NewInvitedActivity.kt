619536900f1995873ac6e6b249637721dee8b331
Mateusz Chabiński <maxteusz@gmail.com>
2022-05-05 15:39:57 +0200

    05.05.2022

--- app/src/main/java/com/example/meetme/Activities/NewInvitedActivity.kt
+++ app/src/main/java/com/example/meetme/Activities/NewInvitedActivity.kt
@@ -2,6 +2,7 @@ package com.example.meetme.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import com.example.meetme.Controllers.SaveInvitedController
 import com.example.meetme.R
@@ -10,7 +11,10 @@ class NewInvitedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-        val spinner_alcokohol : Array<out String> = findViewById<AutoCompleteTextView?>(R.id.alcohol_list).resources.getStringArray(R.array.alcohol_array)
+        val spinner_alcokohol : AutoCompleteTextView = findViewById<AutoCompleteTextView?>(R.id.alcohol_list)
+        val alcohol_array = resources.getStringArray(R.array.alcohol_array)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, alcohol_array)
+        spinner_alcokohol.setAdapter(adapter)
         val saveInvitedController = SaveInvitedController(this)
         saveInvitedController.saveTest()
     }
