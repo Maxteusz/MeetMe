238f7a09a9335ce702097be48cb9fca6486b24ee
Mateusz Chabiński <maxteusz@gmail.com>
2022-10-04 07:47:17 +0200

    3.10

--- app/src/main/java/com/example/meetme/Controllers/MenuActivityController.kt
+++ app/src/main/java/com/example/meetme/Controllers/MenuActivityController.kt
@@ -3,6 +3,7 @@ package com.example.meetme.Controllers
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.Intent
+import android.content.SharedPreferences
 import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
@@ -24,7 +25,9 @@ class MenuActivityController {
 
     constructor(menuActivity: MenuActivity) {
         this.menuActivity = menuActivity
-        StartUpController.currentUser?.token = MyFirebaseMessagingService.fcmToken!!
+        val sharedPreferences : SharedPreferences  = menuActivity.getSharedPreferences("_", MODE_PRIVATE)
+        StartUpController.currentUser?.token = sharedPreferences.getString("FcmToken", "").toString()
+        println("Token:" + StartUpController.currentUser?.token.toString())
     }
 
     fun addInvitedActivity() {
